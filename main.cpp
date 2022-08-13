f8efe834af9a15094525c847463b5659885773bd
Jerry <gjerrys@outlook.com>
2022-08-13 16:24:45 -0400

    update to cmake & add hack sceneviewer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include "QtGui/qsurfaceformat.h"
 #include "ui/MainWindow.h"
 
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +15,14 @@ int main(int argc, char *argv[])
     // Anyway, we will figure this out :).
 
     QApplication a(argc, argv);
+
+    // Set OpenGL version to 4.1 and context to Core
+    QSurfaceFormat fmt;
+    fmt.setVersion(4, 1);
+    fmt.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(fmt);
+
+
     MainWindow w;
     w.show();
     return a.exec();
--- ui/GLWidget.h
+++ ui/GLWidget.h
@@ -1,21 +1,45 @@
 #ifndef GLWIDGET_H
 #define GLWIDGET_H
 
+#include "parser/SceneParser.h"
+#include <QOpenGLBuffer>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
 #include <QOpenGLWidget>
-
-struct RenderData;
-
 
 class GLWidget : public QOpenGLWidget
 {
 public:
     GLWidget(QWidget *parent) : QOpenGLWidget(parent) { }
+    ~GLWidget();
 
     void loadScene(const RenderData &renderData);
 
 protected:
     void initializeGL() override;
     void paintGL() override;
+    void resizeGL(int w, int h) override;
+
+private:
+    QOpenGLShaderProgram *m_program = nullptr;
+
+    QOpenGLVertexArrayObject m_vaoCone;
+    QOpenGLVertexArrayObject m_vaoCube;
+    QOpenGLVertexArrayObject m_vaoCylinder;
+    QOpenGLVertexArrayObject m_vaoSphere;
+
+    QOpenGLBuffer m_vboCone;
+    QOpenGLBuffer m_vboCube;
+    QOpenGLBuffer m_vboCylinder;
+    QOpenGLBuffer m_vboSphere;
+
+    glm::mat4x4 m_view;
+    glm::mat4x4 m_proj;
+    float m_fovy;
+    QVector3D m_lightPos;
+
+    RenderData m_renderData;
 };
 
 #endif // GLWIDGET_H
